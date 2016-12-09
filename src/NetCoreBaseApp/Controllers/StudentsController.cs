fb99cfd944f9d9f725d077afe22698c7aae6d9b3
Jon Munoa <jon.munoa@bq.com>
2016-12-09 10:10:45 +0100

    .NET Core API with Swagger

---
+++ src/NetCoreBaseApp/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.SwaggerGen.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NetCoreBaseApp.Controllers
+{
+    /// <summary>
+    /// Esta clase se usa como CRUD de la entidad alumno
+    /// </summary>
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        /// <summary>
+        /// Este método devuelve una lista de alumnos
+        /// </summary>
+        /// <returns>Lista de alumnos</returns>
+        [SwaggerResponse(200, Description = "Lista de alumnos devuelta", Type = typeof(IEnumerable<string>))]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "Alumno 1", "Alumno 2" });
+        }
+
+        /// <summary>
+        /// Crea un alumno
+        /// </summary>
+        /// <param name="alumno">Nombre del alumno</param>
+        [SwaggerResponse(201, Description = "Alumno creado", Type = typeof(string))]
+        [HttpPost]
+        public IActionResult Post([FromBody]string alumno)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/NetCoreBaseApp/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.Swagger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBaseApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
+            services.AddMvc();
+
+            // Configuración Swagger
+            var pathToDoc = PlatformServices.Default.Application.ApplicationBasePath;
+            services.AddMvc();
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Título de la API",
+                    Description = "Descripción de nuestra API.",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Jon Muñoa", Url = "www.jonmunoa.com" },
+                });
+                options.IncludeXmlComments(pathToDoc + "/NetCoreBaseApp.xml");
+                options.DescribeAllEnumsAsStrings();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseApplicationInsightsRequestTelemetry();
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseMvc();
+
+            // Usar Swagger
+            app.UseSwagger();
+            app.UseSwaggerUi();
+        }
+    }
+}
